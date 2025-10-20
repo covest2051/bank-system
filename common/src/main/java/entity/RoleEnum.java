ff633b9a07212471e75f776c5df14793a265c490
covest2051 <maratpetrovitch@gmail.com>
2025-10-20 20:46:50 +0300

    add jwt-spring-secutiry

---
+++ common/src/main/java/entity/RoleEnum.java
@@ -0,0 +1,8 @@
+package entity;
+
+public enum RoleEnum {
+    CURRENT_CLIENT,
+    BLOCKED_CLIENT,
+    MASTER,
+    GRAND_EMPLOYEE
+}
--- service-client-processing/src/main/java/entity/User.java
+++ service-client-processing/src/main/java/entity/User.java
@@ -2,15 +2,22 @@ package entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -33,4 +40,10 @@ public class User {
 
     @Column(nullable = false, unique = true)
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 }
